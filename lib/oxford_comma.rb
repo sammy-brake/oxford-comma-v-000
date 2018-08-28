5fc5ad66911af447d3e8c6ea680b2bcd97779aa4
Samantha brake <sammy.brake@gmail.com>
2018-08-28 20:47:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  new_string = ""
+  if array.size == 1 
+    return array.join 
+    
+ elsif array.size == 2 
+    
+new_string << array.join(" and ")
 
-end
+else 
+ last_word = array.pop
+  new_string = array.join(", ")
+  
+  new_string << ", and #{last_word}"
+  
+  
+
+end 
+return new_string
+end 
