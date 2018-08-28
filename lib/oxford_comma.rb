307a24c47619d8dddb3f59dafb02cc3977c6eed3
Samantha brake <sammy.brake@gmail.com>
2018-08-28 20:40:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   new_string = ""
-  if array.size == 2 
+  if array.size == 1 
+    return array.join 
+    
+ elsif array.size == 2 
     
 new_string << array.join(" and ")
 
